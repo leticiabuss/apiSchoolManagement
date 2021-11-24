a1c05667eae6cc6b0aa6011522aee631021ce653
Leticia Buss <leticia.souzabuss@gmail.com>
2021-11-23 21:25:49 -0300

    Starting the project

---
+++ src/main/java/com/projeto/apiSchoolManagement/apiSchoolManagementApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.apiSchoolManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class apiSchoolManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run( apiSchoolManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto/apiSchoolManagement/EscolaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.apiSchoolManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
